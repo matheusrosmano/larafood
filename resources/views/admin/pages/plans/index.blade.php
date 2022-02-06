c595a4bd60e8fd132f93d57646a7aa6711775293
Matheus <matheus.rosmaninho@educbank.com.br>
2022-02-06 01:32:45 -0300

    Cria listagem dos planos

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,54 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Filtros
+        </div>
+       <div class="card-body">
+           <table class="table table-condensed">
+               <thead>
+                   <tr>
+                       <th>Nome</th>
+                       <th>Preço</th>
+                       <th>Ações</th>
+                   </tr>
+               </thead>
+               <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>
+                                {{ $plan->name }}
+                            </td>
+                            <td>
+                                {{ $plan->price }}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-warning">
+                                    Ação
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+               </tbody>
+           </table>
+       </div>
+       <div class="card-footer">
+           {!! $plans->links() !!}
+       </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
